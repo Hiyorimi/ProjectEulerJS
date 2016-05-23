b556f943b88096cc0e275cf37cd20fb051bcd9f9
Hiyorimi <playittodeath@gmail.com>
2016-05-23 13:23:40 +0300

    Day 12, 3 problems

---
+++ 28.js
@@ -0,0 +1,54 @@
+'use strict'
+
+function Problem (problem_text) {
+  this._problem_text = problem_text;
+}
+
+Problem.prototype.getProblemText = function () {
+  return this._problem_text;
+}
+
+Problem.prototype.getSolution = function () {
+  return 0;
+}
+
+Problem.prototype.solve = function () {
+  return this.getProblemText() + "\nAnswer: " + this.getSolution();
+}
+
+function Problem28 (problem_text) {
+  this._problem_text = problem_text;
+}
+
+Problem28.prototype = Object.create(Problem.prototype);
+Problem28.prototype.constructor = Problem28;
+
+Problem28.prototype.getSolution = function () {
+
+  var result = 101, square = 0, p = 0;
+  for (var i=7; i<1002;i+=2){
+    square = (i-2)*(i-2);
+    p = i-1; // square - 1 size
+    //(square+i-1) + ((square+i-1) + (i-1)) + ((square+i-1) + (i-1) + (i-1)) + (square+4*(i-1)) =  4 * sqaure + 10 * (i-1)
+    result += 4 * square + 10 * p;
+  }
+
+  return result;
+}
+
+var problem_text = `
+Starting with the number 1 and moving to the right in a clockwise direction a 5 by 5 spiral is formed as follows:
+\n
+21 22 23 24 25
+20  7  8  9 10
+19  6  1  2 11
+18  5  4  3 12
+17 16 15 14 13
+\n
+It can be verified that the sum of the numbers on the diagonals is 101.
+\n
+What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed in the same way?`;
+
+var problem = new Problem28(problem_text);
+
+console.log (problem.solve());
---
+++ 30.js
@@ -0,0 +1,62 @@
+'use strict'
+
+function Problem (problem_text) {
+  this._problem_text = problem_text;
+}
+
+Problem.prototype.getProblemText = function () {
+  return this._problem_text;
+}
+
+Problem.prototype.getSolution = function () {
+  return 0;
+}
+
+Problem.prototype.solve = function () {
+  return this.getProblemText() + "\nAnswer: " + this.getSolution();
+}
+
+function Problem29 (problem_text) {
+  this._problem_text = problem_text;
+}
+
+Problem29.prototype = Object.create(Problem.prototype);
+Problem29.prototype.constructor = Problem29;
+
+Problem29.prototype.getSolution = function () {
+
+  var numbers = [];
+  var a_string = '';
+  var power, sum, a, b = 0;
+  for (var a=10; a<1000000; a++){
+      a_string = a.toString();
+      sum = 0;
+      for (var i=0; i<a_string.length;i++) {
+        power = +a_string[i];
+        sum += Math.pow(power,5);
+      }
+      if (sum==a) {
+        numbers.push(a);
+      }
+  }
+
+  return numbers.reduce(function (sum,elem) {
+    return sum + elem;
+  });
+}
+
+var problem_text = `
+Surprisingly there are only three numbers that can be written as the sum of fourth powers of their digits:
+\n
+1634 = 14 + 64 + 34 + 44
+8208 = 84 + 24 + 04 + 84
+9474 = 94 + 44 + 74 + 44
+As 1 = 14 is not a sum it is not included.
+\n
+The sum of these numbers is 1634 + 8208 + 9474 = 19316.
+\n
+Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.`;
+
+var problem = new Problem29(problem_text);
+
+console.log (problem.solve());
