89ac91bf53c198521431d9f3a95f3c685c8d91e0
Hiyorimi <playittodeath@gmail.com>
2016-06-16 01:57:46 +0600

    Day 32, 243rd problem, achievement

---
+++ 243.js
@@ -0,0 +1,140 @@
+'use strict'
+
+/**
+     * Sets Problem
+     * @class Class for projecteuler problem
+     * @param {String} problem_text Task as text
+     * @param {Object} arguments Arguments array 
+     */
+function Problem (problem_text, input_arguments) {
+  this._problem_text = problem_text;
+  this._arguments = input_arguments; 
+}
+
+/**
+ * getProblemText() returns text of the problem
+ *
+ * @return {String} this._problem_text
+ */
+Problem.prototype.getProblemText = function () {
+  return this._problem_text;
+}
+
+/**
+ * getSolution() is an abstract function for calculating the answer
+ *
+ * @return {Int} 0
+ */
+Problem.prototype.getSolution = function () {
+  return 0;
+}
+
+/**
+ * solve() prints _problem_text and answer, for which calls this.getSolution()
+ * as String
+ *
+ * @return Nan
+ */
+Problem.prototype.solve = function () {
+  console.log(this.getProblemText() + "\nAnswer: " + this.getSolution());
+}
+
+
+function Problem243 (problem_text, input_arguments) {
+  Problem.apply(this, arguments);
+  this._primes = this.sieveOfEratosthenes(1000);
+}
+
+Problem243.prototype = Object.create(Problem.prototype);
+Problem243.prototype.constructor = Problem243;
+
+Problem243.prototype.is_prime = function (a) {
+  return (this._primes.indexOf(a) > 0);
+}
+
+/**
+ * sieveOfEratosthenes(max) returns primes below max
+ * From http://stackoverflow.com/questions/15491291/sieve-of-eratosthenes-algorithm-in-javascript-running-endless-for-large-number
+ *
+ * @param {Int} max
+ * @return {Array} output
+ */
+Problem243.prototype.sieveOfEratosthenes = function (max) {
+    // Eratosthenes algorithm to find all primes under max
+    let array = [], upperLimit = Math.sqrt(max), output = [];
+
+    // Make an array from 2 to (n - 1)
+    for (let i = 0; i < max; i++) {
+        array.push(true);
+    }
+
+    // Remove multiples of primes starting from 2, 3, 5,...
+    for (let i = 2; i <= upperLimit; i++) {
+        if (array[i]) {
+            for (let j = i * i; j < max; j += i) {
+                array[j] = false;
+            }
+        }
+    }
+
+    // All array[i] set to true are primes
+    for (let i = 2; i < max; i++) {
+        if(array[i]) {
+            output.push(i);
+        }
+    }
+
+    return output;
+}
+
+/**
+ * getSolution() returns solution of problem
+ *
+ * @return {Int} denominator
+ */
+Problem243.prototype.getSolution = function () {
+
+  let Fraction = require('fraction.js');
+
+  //From https://github.com/nayuki/Project-Euler-solutions/blob/master/python/p243.py
+  let TARGET = Fraction(15499).div(94744);
+  let totient = 1;
+  let denominator = 1;
+  let p = 2, numer = 0, denom = 0;
+
+  while (true) {
+    totient *= p - 1;
+    denominator *= p;
+
+    while (true) {
+      p += 1
+      if (this.is_prime(p))
+        break
+    }
+    
+    if (Fraction(totient).div(denominator) < TARGET)
+      for (let i = 1; 1 < p; i++) {
+        numer = i * totient;
+        denom = i * denominator;
+        if (Fraction(numer).div(denom - 1) < TARGET)
+          return denom
+      }
+  }
+}
+
+var problem_text = `
+A positive fraction whose numerator is less than its denominator is called a proper fraction.
+For any denominator, d, there will be d−1 proper fractions; for example, with d = 12:
+1/12 , 2/12 , 3/12 , 4/12 , 5/12 , 6/12 , 7/12 , 8/12 , 9/12 , 10/12 , 11/12 .
+\n
+We shall call a fraction that cannot be cancelled down a resilient fraction.
+Furthermore we shall define the resilience of a denominator, R(d), to be the ratio of its proper fractions that are resilient; for example, R(12) = 4/11 .
+In fact, d = 12 is the smallest denominator having a resilience R(d) < 4/10 .
+\n
+Find the smallest denominator d, having a resilience R(d) < 15499/94744 .`;
+
+
+var problem = new Problem243(problem_text, process.argv.splice(2,process.argv.length-1));
+
+
+problem.solve();
